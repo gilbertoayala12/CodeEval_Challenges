225eccc8c5fc502cacd00692e95d8ccc3d367bb5
gilbertoayala12 <gilbertoayala12@gmail.com>
2016-04-19 09:41:39 -0700

    Create Trailing_Strings.java

---
+++ src/Trailing_Strings.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class Main {
+    public static void main (String[] args) throws IOException {
+        File file = new File(args[0]);
+        BufferedReader buffer = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = buffer.readLine()) != null) {
+            line = line.trim();
+            // Process line of input Here
+            String lista[]= line.split(",");
+            
+            if (lista[0].endsWith(lista[1])) {
+                System.out.println("1");
+                
+            }else{
+                System.out.println("0");
+            }
+            
+        }
+    }
+}
