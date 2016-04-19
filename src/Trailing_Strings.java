b8a05161f22a771e979f0c9cb7ce7da21a1ead30
gilbertoayala12 <gilbertoayala12@gmail.com>
2016-04-19 09:42:14 -0700

    Update Trailing_Strings.java

--- src/Trailing_Strings.java
+++ src/Trailing_Strings.java
@@ -1,7 +1,7 @@
 import java.io.*;
 public class Main {
     public static void main (String[] args) throws IOException {
-        File file = new File(args[0]);
+        File file = new File(args[0]);//change args[0] to the path of the file you're reading :d
         BufferedReader buffer = new BufferedReader(new FileReader(file));
         String line;
         while ((line = buffer.readLine()) != null) {
