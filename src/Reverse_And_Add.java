dcbc29bd552a993553215efffb38222994c4ceb3
gilberto ayala <gilbertoayala12@gmail.com>
2017-03-01 21:22:36 -0700

    Adding challenge 45 in java :godmode: :rage: :sushi:

---
+++ src/Reverse_And_Add.java
@@ -0,0 +1,51 @@
+import java.io.*;
+public class Main {
+    public static int reversed(int num) {
+        int reversed = 0;
+
+        while (num != 0) {
+            reversed = reversed * 10;
+            reversed = reversed + num % 10;
+            num = num / 10;
+        }
+
+
+        return reversed;
+    }
+
+    public static boolean isPrime(int num) {
+        if (num == reversed(num)) {
+            return true;
+        } else {
+            return false;
+
+        }
+
+
+    }
+    public static void main (String[] args) throws IOException {
+        File file = new File(args[0]);
+        BufferedReader buffer = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = buffer.readLine()) != null) {
+            line = line.trim();
+            int num = Integer.parseInt(line);
+            int reversed = reversed(num);
+            int suma=0,cont=0;
+            while (true) {
+            suma = num + reversed;
+            cont++;
+            if (isPrime(suma)) {
+                System.out.println(cont + " " + suma);
+                break;
+            } else {
+                num= suma;
+                reversed = reversed(suma);
+
+            }
+
+
+            }
+        }
+    }
+}
